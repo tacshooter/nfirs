4400db992d678d589ea3ec33995ab6d96275e832
Ken Ferguson <cfdev@fergusonhouse.com>
2014-08-30 21:24:54 +0000

    Added api for incidents.

---
+++ api/controllers/IncidentsController.js
@@ -0,0 +1,11 @@
+/**
+ * IncidentsController
+ *
+ * @description :: Server-side logic for managing incidents
+ * @help        :: See http://links.sailsjs.org/docs/controllers
+ */
+
+module.exports = {
+	
+};
+
