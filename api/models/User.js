83c9064fe8e0db8795c495b81664964f45a3979b
tacshooter <ksferg_99@hotmail.com>
2014-08-31 02:00:38 +0000

    Defined User model

--- api/models/User.js
+++ api/models/User.js
@@ -8,7 +8,24 @@
 module.exports = {
 
   attributes: {
-
+    name: 'string',
+    createdAt: {
+      type: 'datetime',
+      defaultsTo: new Date(),
+    }
+    updatedAt: {
+      type: 'datetime',
+      defaultsTo: new Date(),
+    }
+    email: 'string',
+    type: 'string',
+    fname: 'string',
+    lname: 'string',
+    dob: 'array',
+    empnum: 'string',
+    assignment: 'string',
+    shift: 'string',
+    rank: 'string'
   }
 };
 
